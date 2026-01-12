f8e579e39462b928693dba0e3f25cfead063f9df
DANIEL <dandrichest@gmail.com>
2026-01-12 12:09:56 +0100

    Hardcode string

--- app/src/main/java/com/example/helloworldapp/MainActivity.kt
+++ app/src/main/java/com/example/helloworldapp/MainActivity.kt
@@ -6,13 +6,13 @@ import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState: Bundle?)
+        super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         // Find the TextView by the ID we just added
         val helloText = findViewById<TextView>(R.id.helloTextView)
         
-        // Change the text dynamically
-        helloText.text = "Hello from Kotlin Code!"
+        // Change the text dynamically using a string resource
+        helloText.setText(R.string.hello_message)
     }
 }
